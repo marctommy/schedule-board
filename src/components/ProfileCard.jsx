2b3900d1e026c927eca7a0139ad100df280592db
Marc Wiesener <wiesener.marc@gmail.com>
2023-01-19 22:43:41 +0100

    profile card draft done

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import "./ProfileCard.css";
+import photo from "../assets/franzi.jpg";
 
 export default function ProfileCard() {
   return (
-    <div>ProfileCard</div>
-  )
+    <div className="card-container">
+      <header>
+        <img src={photo} alt="photo-name" />
+      </header>
+      <h1 className="bold-text">Name:</h1>
+      <h1 className="normal-text">Subject:</h1>
+    </div>
+  );
 }
